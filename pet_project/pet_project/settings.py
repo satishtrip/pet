85404430defc13ff0bb69849aa0a2f6ba299bf67
satishtrip <44036415+satishtrip@users.noreply.github.com>
2020-11-12 22:01:17 +0530

    Update settings.py

--- pet_project/pet_project/settings.py
+++ pet_project/pet_project/settings.py
@@ -177,5 +177,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'petlookout6918@gmail.com'
-EMAIL_HOST_PASSWORD = 'estophando'
+EMAIL_HOST_PASSWORD = ''
 
